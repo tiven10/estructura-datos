2ad86eea5559c88a262de8ca14a853cfe2a701af
tiven10 <munozvargassteven@gmail.com>
2025-03-29 17:13:09 +0000

    arreglos

--- practicaColas/main.py
+++ practicaColas/main.py
@@ -13,24 +13,23 @@ ticketTypes = {
     "otros": TicketController()
 }
 
-# Endpoint para crear un turno
+
 @app.post("/ticketCreate")
 def crear_turno(turno: Ticket):
-    # Aquí podrías agregar la lógica para guardar el turno en una base de datos
     add_queue(turno, ticketTypes)
     return {"mensaje": "Turno creado correctamente", "datos_turno": turno}
 
-# Endpoint para obtener el siguiente turno
+
 @app.get("/ticketNext")
 def obtener_siguiente_turno(tipo: str):
     return {"mensaje": "El siguiente turno es", "datos_turno": ""}
 
-# Endpoint para listar los turnos en cola por el tipo de turno
+
 @app.get("/ticketList")
 def listar_turnos_cola(tipo: str):
     return {"mensaje": "Lista de turnos en cola", "datos_turnos": ""}
 
-# Otros endpoints existentes
+
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
